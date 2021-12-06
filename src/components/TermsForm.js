37788eff9d8b4942b889a3ea361fc9e98b97c1bd
dmizrahi-makor <dmizrahi@makor-capital.com>
2021-12-06 13:39:25 +0200

    changed every form  to grid

--- src/components/TermsForm.js
+++ src/components/TermsForm.js
@@ -39,8 +39,8 @@ const useStyles = makeStyles({
     padding: "20px",
   },
   termOfUseContainer: {
-    // background: "#FCFCFC 0% 0% no-repeat padding-box",
-    // border: "1px solid #B9C6CD",
+    background: "#FCFCFC 0% 0% no-repeat padding-box",
+    border: "1px solid #B9C6CD",
     opacity: "1",
     "&.MuiPaper-root.MuiPaper-elevation1.MuiPaper-rounded::-webkit-scrollbar": {
       width: "60px",
@@ -119,62 +119,58 @@ const TermsForm = () => {
       <Grid spacing={0} item className={classes.termOfUseContainer}>
         {/* <Paper style={{ maxHeight: "600px", overflow: "auto" }}> */}
         <Paper>
-          <List style={{ maxHeight: "100%", overflow: "auto" }}>
-            <Box>{}</Box>
+        <List style={{ maxHeight: "100%", overflow: "auto" }}>
+          <Box>{}</Box>
 
-            <Box>
-              <Typography className={classes.termsOfUseList}>
-                1.Acceptance of the Terms
-              </Typography>
-              <Box className={classes.termsOfUseList}>
-                <Typography className={classes.termsOfUseList}>1.1</Typography>{" "}
-                These Terms of Use (the “Terms”) set out the terms and
-                conditions under which you (“You” or “Your” as appropriate) can
-                trade cryptocurrencies, for example Bitcoin or Ethereum
-                (“Crypto” or “Cryptocurrency”) using Enigma Securities Limited
-                as a broker (the “Service”). Enigma Securities Ltd (“Enigma”,
-                “Us”, “We” or “Our” as appropriate) is a company registered in
-                England and Wales with company number 11114339 and registered
-                office located at Gossard House, 7/8 Saville Row, London, W1S
-                3PE, United Kingdom. Enigma is an Appointed Representative of
-                Makor Securities London Ltd which is authorised and regulated
-                (Reference No: 625054) by the Financial Conduct Authority (the
-                “FCA”).
-              </Box>
+          <Box>
+            <Typography className={classes.termsOfUseList}>
+              1.Acceptance of the Terms
+            </Typography>
+            <Box className={classes.termsOfUseList}>
+              <Typography className={classes.termsOfUseList}>1.1</Typography>{" "}
+              These Terms of Use (the “Terms”) set out the terms and conditions
+              under which you (“You” or “Your” as appropriate) can trade
+              cryptocurrencies, for example Bitcoin or Ethereum (“Crypto” or
+              “Cryptocurrency”) using Enigma Securities Limited as a broker (the
+              “Service”). Enigma Securities Ltd (“Enigma”, “Us”, “We” or “Our”
+              as appropriate) is a company registered in England and Wales with
+              company number 11114339 and registered office located at Gossard
+              House, 7/8 Saville Row, London, W1S 3PE, United Kingdom. Enigma is
+              an Appointed Representative of Makor Securities London Ltd which
+              is authorised and regulated (Reference No: 625054) by the
+              Financial Conduct Authority (the “FCA”).
             </Box>
-            <Box>
-              <Box className={classes.termsOfUseList}>
-                <Typography className={classes.termsOfUseList}>1.2</Typography>{" "}
-                You acknowledge and agree that by engaging with Enigma, placing
-                any funds with us to execute transactions or generally using the
-                Service in any way, You will be deemed to have accepted these
-                Terms. You understand that each order submitted to Enigma
-                (whether by voice broking, email or via any other electronic
-                interface that We may provide and/or accept) may result in Your
-                entry into one or more binding transactions (a “Transaction”).
-                You assume full financial and performance responsibility for all
-                such Transactions created as a result of the process set out in
-                these Terms.
-              </Box>
+          </Box>
+          <Box>
+            <Box className={classes.termsOfUseList}>
+              <Typography className={classes.termsOfUseList}>1.2</Typography>{" "}
+              You acknowledge and agree that by engaging with Enigma, placing
+              any funds with us to execute transactions or generally using the
+              Service in any way, You will be deemed to have accepted these
+              Terms. You understand that each order submitted to Enigma (whether
+              by voice broking, email or via any other electronic interface that
+              We may provide and/or accept) may result in Your entry into one or
+              more binding transactions (a “Transaction”). You assume full
+              financial and performance responsibility for all such Transactions
+              created as a result of the process set out in these Terms.
             </Box>
-            <Box>
-              <Typography className={classes.termsOfUseList}>
-                2.Amendment to the Terms
-              </Typography>
-              <Box className={classes.termsOfUseList}>
-                Unless We consider any amendment to be a material change to the
-                Terms (in which case we will provide You with not less than 7
-                (seven) days’ prior notice (a “Notice”)), We reserve the right
-                to amend the Terms at any time without prior notice to You. All
-                changes shall take effect immediately and an updated version of
-                the Terms shall be published on Our website. If You do not agree
-                to any amendment then You should stop using the Service
-                immediately.
-              </Box>
+          </Box>
+          <Box>
+            <Typography className={classes.termsOfUseList}>
+              2.Amendment to the Terms
+            </Typography>
+            <Box className={classes.termsOfUseList}>
+              Unless We consider any amendment to be a material change to the
+              Terms (in which case we will provide You with not less than 7
+              (seven) days’ prior notice (a “Notice”)), We reserve the right to
+              amend the Terms at any time without prior notice to You. All
+              changes shall take effect immediately and an updated version of
+              the Terms shall be published on Our website. If You do not agree
+              to any amendment then You should stop using the Service
+              immediately.
             </Box>
-          </List>
-        </Paper>
-        {/* </Paper> */}
+          </Box>
+        </List>
       </Grid>
       <Grid item xs={12}>
         <Grid container direction="column" className={classes.yesNoContainer}>
