a195277e8fffdd4bc88dd0ac45a70b7e2f51fa79
theExperienced <mizradan@gmail.com>
2021-12-13 21:57:48 +0200

    Fix f_proofs loading problem, plus popover bugs.

--- src/components/InfoPopoverButton.js
+++ src/components/InfoPopoverButton.js
@@ -1,34 +1,53 @@
-import { useEffect, useState, useRef, memo } from "react";
+import { useEffect, useState, useRef, memo } from 'react';
 import {
   Typography,
   Box,
   IconButton,
   Popover,
   makeStyles,
-} from "@material-ui/core";
-import { ReactComponent as InfoSvg } from "../assets/circleoutline.svg";
+} from '@material-ui/core';
+import { ReactComponent as InfoSvg } from '../assets/circleoutline.svg';
 
 const useStyles = makeStyles((theme) => ({
   popoverBox: {
-    position: "relative",
+    position: 'relative',
 
     // transform: "translateY(-2px)",
-    [theme.breakpoints.down("md")]: {
-      justifyContent: "end",
+    [theme.breakpoints.down('md')]: {
+      justifyContent: 'end',
+    },
+    '&:hover > p': {
+      opacity: '1',
+      visibility: 'visible',
+      transform: 'translate(100%, -100%)  rotateZ(.19deg)',
     },
   },
   popover: {
-    zIndex: 1301,
+    position: 'absolute',
+    top: '0',
+    right: '0',
+    transition: 'opacity .2s, transform .6s ease-out',
+    opacity: '0',
+    visibility: 'hidden',
+    pointerEvents: 'none',
+    width: '20rem',
+    fontWeight: '400',
+    transform: 'translate(100%, -98%) rotateZ(.19deg)',
+    backgroundColor: 'white',
+    padding: '.75rem',
+    boxShadow: '0 5px 23px rgba(0,0,0,.1)',
+    borderRadius: '3px',
+    // zIndex: 1301,
   },
   popOverContent: {
-    width: "60vw",
-    willChange: "transform",
-    top: "0px",
-    left: "0px",
-    transform: "translate3d(-20px, 10px, 0px)",
-    "& .MuiPopover-paper": {
-      top: "00px",
-      left: "1000px",
+    width: '30rem',
+    // willChange: 'transform',
+    // top: '0px',
+    // left: '0px',
+    transform: 'translate3d(-20px, 10px, 0px)',
+    '& .MuiPopover-paper': {
+      // top: "00px",
+      // left: "1000px",
     },
   },
 }));
@@ -51,35 +70,42 @@ const InfoPopoverButton = (props) => {
   };
 
   const open = !!anchor;
-  const id = open ? "simple-popover" : undefined;
+  const id = open ? 'simple-popover' : undefined;
 
   return (
     <Box className={classes.popoverBox}>
       <IconButton
-        style={{ padding: 0, marginLeft: "10px" }}
+        style={{ padding: 0, marginLeft: '10px' }}
         onMouseEnter={handleClick}
+        onMouseLeave={handleClose}
         // className={anchor ? classes.popover : ""} ///////////////CHECK FURTHER
       >
         <InfoSvg />
       </IconButton>
 
+      <Typography className={classes.popover}>{props.info}</Typography>
+
+      {/* 
       <Popover
-        id={id}
+        id='mouse-over-popover'
+        sx={{
+          pointerEvents: 'none',
+        }}
         className={classes.popOverContent}
         open={open}
         anchorEl={anchor}
         onClose={handleClose}
         anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "center",
+          vertical: 'bottom',
+          horizontal: 'left',
         }}
         transformOrigin={{
-          vertical: "top",
-          horizontal: "center",
+          vertical: 'top',
+          horizontal: 'left',
         }}
-      >
-        <Typography sx={{ p: 2 }}>{props.info}</Typography>
-      </Popover>
+      > 
+        <Typography sx={{ pt: 2 }}>{props.info}</Typography>
+      </Popover>*/}
     </Box>
   );
 };
